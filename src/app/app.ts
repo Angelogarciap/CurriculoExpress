024a9ad36267cdc2d831711694f41abb31227273
Angelogarciap <angelogarcia3237@gmail.com>
2025-10-13 17:13:14 -0400

    adicionando form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { Form } from './pages/form/form';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent], // apenas importa para usar <app-home>
-  template: `<app-home></app-home>`,
-  standalone: true
+  imports: [RouterOutlet, HomeComponent, Form],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('gerador_curriculo');
--- src/app/pages/form/form.spec.ts
+++ src/app/pages/form/form.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Home } from './home';
+import { Form } from './form';
 
-describe('Home', () => {
-  let component: Home;
-  let fixture: ComponentFixture<Home>;
+describe('Form', () => {
+  let component: Form;
+  let fixture: ComponentFixture<Form>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      imports: [Form]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Home);
+    fixture = TestBed.createComponent(Form);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
